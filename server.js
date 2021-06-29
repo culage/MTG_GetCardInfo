c8ecb433398827b3cbc31efbd7304e6431ad5ebf
culage <culage@gmail.com>
2021-06-29 20:40:19 +0900

    とりあえず検索して追加できる

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var http = require('http');
-var indexHtml = require('fs').readFileSync('index.html');
 var whisper = require('./get_card.js');
 
 var server = http.createServer();
 
 server.on('request', async function(req, res) {
 	if (req.url == "/") {
+		var indexHtml = require('fs').readFileSync('./index.html');
 		res.writeHead(200,{'Content-Type':'text/html'});
 		res.end(indexHtml);
+		return;
+	}
+	if (req.url == "/lib/vue.min.js") {
+		var vueScript = require('fs').readFileSync('./lib/vue.min.js');
+		res.writeHead(200,{'Content-Type':'text/javascript'});
+		res.end(vueScript);
 		return;
 	}
 	if (req.url.startsWith("/get_card?q=")) {
